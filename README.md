468da81b4a7805268b60598be21183a2e5f2248b
SkandaAithal-1 <aithalskanda66@gmail.com>
2025-05-14 17:59:16 +0530

    Task 02: Matrix Addition

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ This is to serve as the documentation for CUDA learning journey. Studied from th
 
 **Summary** :  
 Implemented a simple kernel to add two vectors. Explored launching kernels, device memory management, data transfer between host and device and basic CUDA programming concepts.
+
+---
+
+## Task 02: Matrix Addition
+
+**Summary** :  
+Implemented a kernel to add two matrices. Learnt about barrier synchronisation, thread scheduling, hardware resource management (warps and stuff) and latency tolerance.
---
+++ Task02/matrix_addition.cu
@@ -0,0 +1,47 @@
+#include <iostream>
+
+__global__ void matrix_addition(const float *A, const float *B, float *C, int M, int N)
+{
+    int row = blockIdx.x * blockDim.x + threadIdx.x;
+    int col = blockIdx.y * blockDim.y + threadIdx.y;
+    if (row < M && col < N)
+    {
+        C[row * N + col] = A[row * N + col] + B[row * N + col];
+    }
+}
+
+int main()
+{
+    const int M = 1024;
+    const int N = 1024;
+    float A[M * N], B[M * N], C[M * N];
+    float *Ad, *Bd, *Cd;
+    size_t size = M * N * sizeof(float);
+
+    // Allocate memory on the device
+    cudaMalloc((void **)&Ad, size);
+    cudaMalloc((void **)&Bd, size);
+    cudaMalloc((void **)&Cd, size);
+
+    // Copy data from host to device
+    cudaMemcpy(Ad, A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(Bd, B, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(Cd, C, size, cudaMemcpyHostToDevice);
+
+    // Execution parameter configuration
+    dim3 blockDim(16, 16);
+    dim3 gridDim(ceil(M / blockDim.x), ceil(N / blockDim.y));
+
+    // Launch kernel
+    matrix_addition<<<gridDim, blockDim>>>(Ad, Bd, Cd, M, N);
+
+    // Copy result from device to host
+    cudaMemcpy(C, Cd, size, cudaMemcpyDeviceToHost);
+
+    // Free device memory
+    cudaFree(Ad);
+    cudaFree(Bd);
+    cudaFree(Cd);
+
+    return 0;
+}
