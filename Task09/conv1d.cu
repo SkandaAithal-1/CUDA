0aec2e86e0a5cd4fbd5bb436a35bf2c6b1968b8a
SkandaAithal-1 <aithalskanda66@gmail.com>
2025-06-13 21:20:02 +0530

    Optimize conv1d with shared memory

--- Task09/conv1d.cu
+++ Task09/conv1d.cu
@@ -1,6 +1,10 @@
 #include <iostream>
 
-__global__ void conv1d(const float *a, const float *b, float *c, int N, int K)
+const int TILE_WIDTH = 256;
+const int KERNEL_WIDTH = 5;
+__constant__ float kernel[KERNEL_WIDTH];
+
+__global__ void conv1d_unoptim(const float *a, const float *b, float *c, int N, int K)
 {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < N - K + 1)
@@ -14,12 +18,50 @@ __global__ void conv1d(const float *a, const float *b, float *c, int N, int K)
     }
 }
 
-void launch_conv1d(const float *a, const float *b, float *c, int N, int K)
+__global__ void conv1d(const float *in, float *out, int N, int K)
+{
+    int tx = threadIdx.x, ty = threadIdx, y;
+    int idx = blockIdx.x * blockDim.x + tx;
+    __shared__ float sharedMemIn[TILE_WIDTH];
+    extern __constant__ float kernel[];
+    if (idx < N)
+    {
+        sharedMemIn[tx] = in[idx];
+        __syncthreads();
+
+        float result = 0.0f;
+        for (int i=min(idx, KERNEL_WIDTH); i>=max(idx-K+1, 0); i--){
+            out[idx-i] += sharedMemIn[tx] * kernel[i];
+        }
+    }
+}
+
+void launch_conv1d(const float *a, const float *b, float *c, int N, int K){
+    float *ad, *cd;
+    cudaMalloc((void**)&ad, N * sizeof(float));
+    cudaMalloc((void **)&cd, (N - K + 1) * sizeof(float));
+
+    __constant__ float kernel[K];
+    cudaMemcpy(ad, a, N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(kernel, b, N * sizeof(float), cudaMemcpyHostToDevice);
+
+    int blockSize = TILE_WIDTH;
+    int gridSize = (N + blockSize - 1) / blockSize;
+
+    conv1d<<<gridSize, blockSize>>>(ad, cd, N, K);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(c, cd, (N - K + 1)*sizeof(float), cudaMemcpyDeviceToHost);
+    cudaFree(ad);
+    cudaFree(cd);
+}
+
+void launch_conv1d_unoptim(const float *a, const float *b, float *c, int N, int K)
 {
     float *ad, *bd, *cd;
-    cudaMalloc((void**)&ad, N * sizeof(float));
-    cudaMalloc((void**)&bd, K * sizeof(float));
-    cudaMalloc((void**)&cd, (N - K + 1) * sizeof(float));
+    cudaMalloc((void **)&ad, N * sizeof(float));
+    cudaMalloc((void **)&bd, K * sizeof(float));
+    cudaMalloc((void **)&cd, (N - K + 1) * sizeof(float));
 
     cudaMemcpy(ad, a, N * sizeof(float), cudaMemcpyHostToDevice);
     cudaMemcpy(bd, b, K * sizeof(float), cudaMemcpyHostToDevice);
@@ -27,7 +69,7 @@ void launch_conv1d(const float *a, const float *b, float *c, int N, int K)
     int blockSize = 256;
     int gridSize = (N - K + 1 + blockSize - 1) / blockSize;
 
-    conv1d<<<gridSize, blockSize>>>(ad, bd, cd, N, K);
+    conv1d_unoptim<<<gridSize, blockSize>>>(ad, bd, cd, N, K);
     cudaDeviceSynchronize();
 
     cudaMemcpy(c, cd, (N - K + 1) * sizeof(float), cudaMemcpyDeviceToHost);
@@ -43,11 +85,14 @@ int main()
     float a[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     float b[K] = {1, 0, -1};
     float c[N - K + 1];
+    float c_optim[N - K + 1] = {0.0f};
 
-    launch_conv1d(a, b, c, N, K);
+    launch_conv1d_unoptim(a, b, c, N, K);
+
+    launch_conv1d(a, b, c_optim, N, K);
 
     std::cout << "Convolution result: ";
-    for (int i=0; i < N - K + 1; i++)
+    for (int i = 0; i < N - K + 1; i++)
     {
         std::cout << c[i] << " ";
     }
