3f75fede2f340ccddba8e55a3d7872cf0c710c6d
SkandaAithal-1 <aithalskanda66@gmail.com>
2025-06-15 12:30:33 +0530

    Optimize convolution with tiling (input)

--- Task09/conv1d.cu
+++ Task09/conv1d.cu
@@ -1,6 +1,10 @@
 #include <iostream>
 
-const int TILE_WIDTH = 256;
+// Assumptions : 
+// KERNEL_WIDTH is small and can fit in connstant memory
+// KERNEL_WIDTH is odd
+
+const int INPUT_TILE_WIDTH = 256;
 const int KERNEL_WIDTH = 5;
 __constant__ float kernel[KERNEL_WIDTH];
 
@@ -18,40 +22,45 @@ __global__ void conv1d_unoptim(const float *a, const float *b, float *c, int N, 
     }
 }
 
-__global__ void conv1d(const float *in, float *out, int N, int K)
+__global__ void conv1d(const float *in, float *out, int N)
 {
-    int tx = threadIdx.x, ty = threadIdx, y;
-    int idx = blockIdx.x * blockDim.x + tx;
-    __shared__ float sharedMemIn[TILE_WIDTH];
-    extern __constant__ float kernel[];
-    if (idx < N)
-    {
-        sharedMemIn[tx] = in[idx];
-        __syncthreads();
+    int tx = threadIdx.x;
+    int bx = blockIdx.x;
+    int inputIdx = bx * blockDim.x + tx;
+    int radius = KERNEL_WIDTH/2;
 
-        float result = 0.0f;
-        for (int i=min(idx, KERNEL_WIDTH); i>=max(idx-K+1, 0); i--){
-            out[idx-i] += sharedMemIn[tx] * kernel[i];
+    __shared__ float sharedMem[INPUT_TILE_WIDTH];
+
+    if (inputIdx < N) sharedMem[tx] = in[inputIdx];
+    __syncthreads();
+
+    int outputIdx = inputIdx - radius;
+    if (0 <= outputIdx && outputIdx < N - KERNEL_WIDTH + 1)
+    {
+        float outValue = 0.0f;
+        for (int k = 0; k < KERNEL_WIDTH; k++)
+        {
+            outValue += kernel[k] * sharedMem[tx - radius + k];
         }
+        out[outputIdx] = outValue;
     }
 }
 
-void launch_conv1d(const float *a, const float *b, float *c, int N, int K){
+void launch_conv1d(const float *a, const float *b, float *c, int N){
     float *ad, *cd;
     cudaMalloc((void**)&ad, N * sizeof(float));
     cudaMalloc((void **)&cd, (N - K + 1) * sizeof(float));
 
-    __constant__ float kernel[K];
     cudaMemcpy(ad, a, N * sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(kernel, b, N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpyToSymbol(kernel, b, N * sizeof(float), cudaMemcpyHostToDevice);
 
     int blockSize = TILE_WIDTH;
     int gridSize = (N + blockSize - 1) / blockSize;
 
-    conv1d<<<gridSize, blockSize>>>(ad, cd, N, K);
+    conv1d<<<gridSize, blockSize>>>(ad, cd, N);
     cudaDeviceSynchronize();
 
-    cudaMemcpy(c, cd, (N - K + 1)*sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, cd, (N - KERNEL_WIDTH + 1)*sizeof(float), cudaMemcpyDeviceToHost);
     cudaFree(ad);
     cudaFree(cd);
 }
